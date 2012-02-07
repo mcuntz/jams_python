77215a10a29c01e144ebd6fd4e4bce9320d31e65
Matthias Cuntz <matthias.cuntz@ufz.de>
2012-02-07 12:43:34 +0000

    Some more constants like gas constant R. All constants in docstring now.
    git-svn-id: https://svn.ufz.de/svn/chs-svn/PYTHON_chs_lib@197 20e3f476-f421-4892-ba25-73c5d2d2c40b

--- const.py
+++ const.py
@@ -5,19 +5,22 @@
     Definition
     ----------
     Pi = 3.141592653589793238462643383279502884197
-    ....
+    ...
+    Define the following constants:
+        Mathematical
+            Pi, Pi2, TwoPi, Sqrt2
+        Physical
+            Gravity, T0, P0, T25, sigma, R, Na
+        Isotope
+            RPDB
+        Computational
+            tiny
 
 
     Examples
     --------
     >>> print Pi
     3.14159265359
-
-    >>> print Pi2
-    1.57079632679
-
-    >>> print TwoPi
-    6.28318530718
 
     >>> print Sqrt2
     1.41421356237
@@ -27,9 +30,6 @@
 
     >>> print T0
     273.15
-
-    >>> print P0
-    101325.0
 
     >>> print sigma
     5.67e-08
@@ -52,10 +52,13 @@ TwoPi = 6.283185307179586476925286766559005768394    # 2*Pi
 Sqrt2 = 1.41421356237309504880168872420969807856967  # Sqrt(2)
 
 # Physical
-Gravity   = 9.81      # Gravity acceleration [m^2/s]
-T0        = 273.15    # Celcius <-> Kelvin [K]
-P0        = 101325.   # Standard pressure [Pa]
-sigma     = 5.67e-08  # Stefan-Boltzmann constant [W/m^2/K^4]
+Gravity = 9.81          # Standard average Earth's gravity [m^2/s]
+T0      = 273.15        # Celcius <-> Kelvin [K]
+P0      = 101325.       # Standard pressure [Pa]
+T25     = 298.15        # Standard ambient temperature [K]
+sigma   = 5.67e-08      # Stefan-Boltzmann constant [W/m^2/K^4]
+R       = 8.3144621     # Ideal gas constant [J/K/mol]
+Na      = 6.02214129e23 # Avogrado number [mol^-1]
 
 # Isotope
 RPDB = 0.0112372 # Isotope ratio of VPDB-CO2
