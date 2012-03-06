17e6a21594aeebecc3903309623edc9942c22340
Matthias Cuntz <matthias.cuntz@ufz.de>
2012-03-06 17:29:17 +0000

    Save partition
    git-svn-id: https://svn.ufz.de/svn/chs-svn/PYTHON_chs_lib@218 20e3f476-f421-4892-ba25-73c5d2d2c40b

--- partition.py
+++ partition.py
@@ -6,16 +6,18 @@ import scipy.optimize as opt #curve_fit, fmin
 if True:
     import ufz
     dat   = ufz.fread('partition_test.csv', skip=2, transpose=True)
-    dates = ufz.date2dec(dy=dat[0,:], mo=dat[1,:], yr=dat[2,:], hr=dat[3,:], mi=dat[4,:]) 
+    dates = ufz.date2dec(dy=dat[0,:], mo=dat[1,:], yr=dat[2,:], hr=dat[3,:], mi=dat[4,:])
     NEE   = np.squeeze(dat[7,:])
     rg    = np.squeeze(dat[10,:])
-    tair  = np.squeeze(dat[11,:]) + 273.15
-    tsoil = np.squeeze(dat[12,:]) + 273.15
+    tair  = np.squeeze(dat[11,:])
     undef = -9999.
     ii    = np.squeeze(np.where((NEE != undef) & (tair != undef) & (rg != undef)))
+    jul   = dates[ii]
     nee   = NEE[ii]
-    t     = tair[ii]
-    isday = np.where(rg[ii] > 10., 1, 0)
+    t     = tair[ii] + 273.15
+    rad   = rg[ii]
+    isday = np.where(rad > 10., 1, 0)
+    local = True
     """
         Fills gaps of flux data from Eddy covariance measurements according to
         Reichstein et al. (2005).
@@ -101,7 +103,9 @@ if True:
         t0   = 227.13
         return p0*np.exp(p1*(1./(tref-t0)-1./(t-t0)))
 
-    
+    def rref(et, p0):
+        return p0*et
+
     # -------------------------------------------------------------
     # Checks
 
@@ -116,26 +120,104 @@ if True:
     if ((np.size(t) != ndata) | (np.size(isday) != ndata)):
         raise ValueError('Error partition: inputs must have the same size.')
 
-    
+
     # -------------------------------------------------------------
     # Partition
 
-    ii = np.squeeze(np.where(np.array(isday,dtype=np.int) == 0))
-    xx = t[ii]
-    yy = nee[ii]
+    ii  = np.squeeze(np.where(np.array(isday,dtype=np.int) == 0))
+    nii = np.size(ii)
+    jj  = jul[ii] # night time, undef removed julian day
+    tt  = t[ii]   #      "    ,        "      temperature
+    nn  = nee[ii] #      "    ,        "      nee
 
-    plc, ple = opt.curve_fit(lloyd, xx, yy, p0=np.array([2.,200.]))
-    err      = ple * np.sum((yy-lloyd(xx,plc[0],plc[1]))**2)
-    pls      = np.sqrt(np.array([err[0,0],err[1,1]])/(np.size(ii)-2))
-    print 'Curve fit 1: ', plc, ' +- ', pls
+    if not local: # Global relationship for comparison
+        p, c = opt.curve_fit(lloyd, tt, nn, p0=[2.,200.])      # global parameter, global cov matrix
+        #nii  = np.size(tt)
+        #res  = np.sum((nn-lloyd(tt,p[0],p[1]))**2)            # global residuals
+        #s    = np.sqrt(np.diag(c * res)/np.float(nii-2))      # global param error
+        #r    = c[0,1]/np.sqrt(c[0,0]*c[1,1])                  # corr between parameters
+        #print 'Global curve fit: ', p, ' +- ', s, 'corr=', r
+        ndates   = np.size(dates)
+        Reco     = np.ones(ndates)*undef
+        ii       = np.squeeze(np.where((tair != undef)))
+        t        = tair[ii] + 273.15
+        Reco[ii] = lloyd(t, p[0], p[1])
 
-    
-    import matplotlib.pyplot as plt
-    plt.figure(1)
-    plt.plot(xx, yy, 'x', color='b')
-    plt.plot(xx, lloyd(xx,plc[0],plc[1]), 'k:')
-    plt.show()
-    
+    else: # Local relationships
+        locp = [] # local param
+        locs = [] # local err
+        #locc = [] # local cov
+        dmin = np.int(np.floor(np.amin(jj))) # be aware that julian days starts at noon, i.e. 1.0 is 12h
+        dmax = np.int(np.ceil(np.amax(jj)))  # so the search will be from noon to noon and therefore includes total nights
+        for i in xrange(dmin,dmax,5):
+            iii  = np.squeeze(np.where((jj>=i) & (jj<(i+14))))
+            niii = np.size(iii)
+            if niii > 6:
+                if (np.amax(tt[iii])-np.amin(tt[iii])) >= 5.:
+                    p, c = opt.curve_fit(lloyd, tt[iii], nn[iii], p0=[2.,200.])
+                    res  = np.sum((nn[iii]-lloyd(tt[iii],p[0],p[1]))**2)
+                    s    = np.sqrt(np.diag(c * res)/np.float(niii-2))
+                    locp = locp + [p]
+                    locs = locs + [s]
+                    #locc = locc + [c]
+        if len(locp) == 0:
+            raise ValueError('No local relationship found.')
+        locp   = np.squeeze(np.array(locp))
+        locs   = np.squeeze(np.array(locs))
+        #locc   = np.squeeze(np.array(locc))
+        # best local relationship (avg of first 3 best)
+        iii  = np.squeeze(np.where((locp[:,1] > 0.) & (locp[:,1] < 450.) & (np.abs(locs[:,1]/locp[:,1]) < 0.5)))
+        niii = np.size(iii)
+        if niii==0:
+            raise ValueError('No good local relationship found.')
+        elif niii==1:
+            bestp = locp[iii,:]
+            bests = locs[iii,:]
+            #cc    = locc[iii,:]
+        elif niii==2:
+            bestp = np.mean(locp[iii,:],axis=0)
+            bests = np.mean(locs[iii,:],axis=0)
+            #ls    = locs[iii,:]
+            #iis   = np.argsort(ls[:,1])
+            #cc    = locc[iii[iis[0]],:]
+        else:
+            lp  = locp[iii,:]
+            ls  = locs[iii,:]
+            iis = np.argsort(ls[:,1])
+            bestp = np.mean(lp[iis[0:3],:],axis=0)
+            bests = np.mean(ls[iis[0:3],:],axis=0)
+            #cc = locc[iii[iis[0]],:]
+        #corr = cc[0,1]/np.sqrt(cc[0,0]*cc[1,1])
+        #print 'Local best curve fit: ', bestp, ' +- ', bests, 'corr=', corr
+                        
+        refp  = [] # Rref param
+        refii = [] # mean index of data points
+        E0 = bestp[1]
+        et = lloyd(tt, 1., E0)
+        for i in xrange(dmin,dmax,5):
+            iii  = np.squeeze(np.where((jj>=i) & (jj<(i+4))))
+            niii = np.size(iii)
+            if niii > 3:
+                p = np.sum(nn[iii])/np.sum(et[iii])
+                refp  = refp  + [p]
+                refii = refii + [np.int((iii[0]+iii[-1])/2)]
+        if len(refp) == 0:
+            raise ValueError('No ref relationship found.')
+        refp   = np.squeeze(np.array(refp))
+        refii  = np.squeeze(np.array(refii))
+
+        Rref   = np.interp(dates, jj[refii], refp)
+        ndates = np.size(dates)
+        Reco   = np.ones(ndates)*undef
+        ii    = np.squeeze(np.where((tair != undef)))
+        t     = tair[ii] + 273.15
+        Reco[ii]  = lloyd(t, Rref[ii], E0)
+
+    ii   = np.squeeze(np.where((NEE != undef) & (tair != undef)))
+    GPP  = np.ones(ndates)*undef
+    t    = tair[ii] + 273.15
+    GPP[ii]  = Reco[ii] - NEE[ii]
+
 
 if __name__ == '__main__':
     import doctest
