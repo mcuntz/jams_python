dadd9a2bfc27152aa0bea91e8b2ccc01cc9d1b1b
Arndt Piayda <arndt.piayda@ufz.de>
2014-05-28 08:38:36 +0000

    PYTHON_chs_lib/get_nearest.py: Returns a value z for each point in xy which is equal to the nearest point value in the xyz field.
    git-svn-id: https://svn.ufz.de/svn/chs-svn/PYTHON_chs_lib@1572 20e3f476-f421-4892-ba25-73c5d2d2c40b

---
+++ get_nearest.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import numpy as np
+from scipy.spatial.distance import cdist
+
+def get_nearest(xy, xyz):
+    """
+        Returns a value z for each point in xy which is equal to the nearest
+        point value in the xyz field.
+
+
+        Definition
+        ----------
+        get_nearest(xy, xyz):
+
+
+        Input
+        -----
+        xy        2D np.array (n,2),
+                  x and y coordinates of the points where a z value is desired
+        xyz       2D np.array (m,3), x, y and z vaules of the field
+
+
+        Output
+        ------
+        z         1D np.array (n,), z values at all points of xy
+
+
+        Restrictions
+        ------------
+        Does not work with NAN values or masked arrays. You need to provide only
+        valid data.
+
+
+        Example
+        --------
+        # make some data
+        >>> np.random.seed(seed=1)
+        >>> xy = np.random.random(6).reshape(-1,2)
+        >>> np.random.seed(seed=2)
+        >>> xyz = np.random.random(150).reshape(-1,3)
+
+        # get z values at the points xy
+        >>> z = get_nearest(xy, xyz)
+        >>> print(np.round(z,2))
+        [ 0.25  0.7   0.28]
+
+
+        License
+        -------
+        This file is part of the UFZ Python library.
+
+        The UFZ Python library is free software: you can redistribute it and/or modify
+        it under the terms of the GNU Lesser General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        The UFZ Python library is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+        GNU Lesser General Public License for more details.
+
+        You should have received a copy of the GNU Lesser General Public License
+        along with the UFZ makefile project (cf. gpl.txt and lgpl.txt).
+        If not, see <http://www.gnu.org/licenses/>.
+
+        Copyright 2014 Arndt Piayda
+
+
+        History
+        -------
+        Written,  AP, May 2014
+    """
+
+    dist   = cdist(xy, xyz[:,:2], metric='euclidean')
+    dist   = np.ma.array(dist, mask=dist==0.)
+    indmin = np.ma.argmin(dist, axis=1)
+    
+    return xyz[indmin,2]
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(optionflags=doctest.NORMALIZE_WHITESPACE)
