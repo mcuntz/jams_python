472821c8517eb362d471650c00bf1774b50f07f8
Arndt Piayda <arndt.piayda@ufz.de>
2014-11-10 15:26:24 +0000

    PYTHON_chs_lib/ufz/eddybox/fluxpart.py bug fix
    git-svn-id: https://svn.ufz.de/svn/chs-svn/PYTHON_chs_lib@1843 20e3f476-f421-4892-ba25-73c5d2d2c40b

--- ufz/eddybox/fluxpart.py
+++ ufz/eddybox/fluxpart.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import numpy as np
-from ufz.eddybox import nee2gpp
+from eddybox import nee2gpp
 from ufz.esat import esat
 from ufz.date2dec import date2dec
 
@@ -40,6 +40,8 @@ def fluxpart(fluxfile, metfile, outdir, swdr, tair, rh, method='local',
                      if 'local' | 'reichstein',  method of Reichstein et al. (2005)
                      if 'day'   | 'lasslop',     method of Lasslop et al. (2010)
                      (default: 'local')
+    nogppnight  if True:  Resp=NEE, GPP=0 at night, GPP always positive
+                if False: Resp=lloyd_taylor, GPP=Resp-NEE at night (default)
     delimiter   list of str, delimiters of fluxfile and metfile
                 (default: [',',','])
     skiprows    list of int, lines to skip at the beginning of fluxfile and
@@ -80,8 +82,7 @@ def fluxpart(fluxfile, metfile, outdir, swdr, tair, rh, method='local',
     d = np.loadtxt(fluxfile, dtype='|S100', delimiter=delimiter[0], skiprows=skiprows[0])
     m = np.loadtxt(metfile,  dtype='|S100', delimiter=delimiter[1], skiprows=skiprows[1])
     
-    if d.shape[1]!=16:
-        raise ValueError('fluxpart: fluxfile must be from fluxfill and have 16 cols')
+    assert (d.shape[1]==16) | (d.shape[1]==24), 'fluxfill: fluxfile must be from fluxfill and have 16 or 19 cols'
     
     if format[0]=='ascii':
         datev   = date2dec(ascii=d[:,0])
@@ -101,7 +102,11 @@ def fluxpart(fluxfile, metfile, outdir, swdr, tair, rh, method='local',
     
     ###########################################################################
     # assign variables
-    nee       = val[:,9] #corr. C
+    if (d.shape[1]==11):
+        nee       = val[:,9] #corr. C
+    else:
+        nee       = val[:,12] # oder 13 #corr. C
+        nee_stor  = val[:,13] # oder 13 #corr. C
     swdr      = met[:,swdr]
     isday     = swdr>0.
     tair      = met[:,tair]+273.15
@@ -110,22 +115,48 @@ def fluxpart(fluxfile, metfile, outdir, swdr, tair, rh, method='local',
     
     ###########################################################################
     # do partitioning
-    gpp, reco = nee2gpp(datev, nee, tair, isday, rg=swdr, vpd=vpd, undef=undef,
-                        method=method, shape=False, masked=False,
-                        nogppnight=nogppnight)
+    gpp, reco           = nee2gpp(datev, nee,      tair, isday, rg=swdr,
+                                  vpd=vpd, undef=undef, method=method,
+                                  shape=False, masked=False, nogppnight=nogppnight)
+    gpp_stor, reco_stor = nee2gpp(datev, nee_stor, tair, isday, rg=swdr,
+                                  vpd=vpd, undef=undef, method=method,
+                                  shape=False, masked=False, nogppnight=nogppnight)
     
     #######################################################################
     # plot
     if plot:
+        import matplotlib as mpl
         import matplotlib.pyplot as plt
         import matplotlib.backends.backend_pdf as pdf
         pp1 = pdf.PdfPages(outdir+'/fluxpart.pdf')
-
+        
+        majticks = mpl.dates.MonthLocator(bymonthday=1)
+        format_str='%d %m %Y %H:%M'
+        date01 = date2dec(yr=1, mo=1, dy=2, hr=0, mi=0, sc=0)
+        
         fig1 = plt.figure(1)
-        sub1 = fig1.add_subplot(111)
-        l1 =sub1.plot(datev, nee, '-k', label='nee')
-        l2 =sub1.plot(datev, gpp, '-g', label='gpp')
-        l3 =sub1.plot(datev, reco, '-r', label='reco')
+        if (d.shape[1]==11):
+            sub1 = fig1.add_subplot(111)
+        else:
+            sub1 = fig1.add_subplot(211)
+            sub2 = fig1.add_subplot(212)
+        l1 =sub1.plot(datev-date01, nee, '-k', label='nee')
+        l2 =sub1.plot(datev-date01, gpp, '-g', label='gpp')
+        l3 =sub1.plot(datev-date01, reco, '-r', label='reco')
+        if (d.shape[1]!=11):
+            l4 =sub2.plot(datev-date01, nee_stor, '-k', label='nee')
+            l5 =sub2.plot(datev-date01, gpp_stor, '-g', label='gpp')
+            l6 =sub2.plot(datev-date01, reco_stor, '-r', label='reco')
+        
+        sub1.set_xlim(datev[0]-date01,datev[-1]-date01)
+        sub1.xaxis.set_major_locator(majticks)
+        sub1.xaxis.set_major_formatter(mpl.dates.DateFormatter(format_str))
+        if (d.shape[1]!=11):
+            sub2.set_xlim(datev[0]-date01,datev[-1]-date01)
+            sub2.xaxis.set_major_locator(majticks)
+            sub2.xaxis.set_major_formatter(mpl.dates.DateFormatter(format_str))
+        fig1.autofmt_xdate()
+         
         plt.legend(loc='best')
         plt.show()
         fig1.savefig(pp1, format='pdf')
@@ -133,17 +164,33 @@ def fluxpart(fluxfile, metfile, outdir, swdr, tair, rh, method='local',
     
     ###########################################################################
     # prepare output and save file
-    header         = np.array(['          H', '   Hflag', '     Hgf',
-                               '         LE', '  LEflag', '    LEfg',
-                               '          E', '   Eflag', '     Efg',
-                               '          C', '   Cflag', '     Cfg',
-                               '        TAU', ' TAUflag', '   TAUfg',
-                               '        GPP', ' GPPflag', '   GPPfg',
-                               '       Reco', 'Recoflag', '  Recofg'])
-    flux = np.vstack((gpp,reco)).transpose()
-    flux_str       = np.array([['%11.5f'%x for x in y] for y in flux]).repeat(3, axis=1)
-    flux_str[:,[1,2,4,5]] = np.tile(d[:,11:13],2)
-    output         = np.hstack((d[:,:], flux_str))
+    if (d.shape[1]==11):
+        header         = np.array(['          H', '   Hflag', '     Hgf',
+                                   '         LE', '  LEflag', '    LEgf',
+                                   '          E', '   Eflag', '     Egf',
+                                   '          C', '   Cflag', '     Cgf',
+                                   '        TAU', ' TAUflag', '   TAUgf',
+                                   '        GPP', ' GPPflag', '   GPPgf',
+                                   '       Reco', 'Recoflag', '  Recogf'])
+        flux     = np.vstack((gpp,reco)).transpose()
+        flux_str = np.array([['%11.5f'%x for x in y] for y in flux]).repeat(3, axis=1)
+        flux_str[:,[1,2,4,5]] = np.tile(d[:,11:13],2)
+        output   = np.hstack((d[:,:], flux_str))
+    else:
+        header         = np.array(['          H', '       H+sT', '   Hflag', '     Hgf',
+                                   '         LE', '     LE+sLE', '  LEflag', '    LEgf',
+                                   '          E', '       E+sE', '   Eflag', '     Egf',
+                                   '          C', '       C+sC', '   Cflag', '     Cgf',
+                                   '        TAU',    ' TAUflag',             '   TAUgf',
+                                   '         sT', '        sLE', '         sE', '         sC',
+                                   '        GPP', '     GPP+sC', ' GPPflag', '   GPPfg',
+                                   '       Reco', '    Reco+sC', 'Recoflag', '  Recofg'])
+        flux     = np.vstack((gpp,gpp_stor,reco,reco_stor)).transpose()
+        flux_str = np.array([['%11.5f'%x for x in y] for y in flux])
+        flux_str = np.insert(flux_str, [2,2,4,4], flux_str, axis=1)
+        flux_str[:,[2,3,6,7]] = np.tile(d[:,11:13],2)
+        output   = np.hstack((d[:,:], flux_str))
+    
     np.savetxt('%s/fluxpart.csv'%outdir,
                np.vstack((np.concatenate((['            date'], header))[np.newaxis,:],
                           output)), '%s', delimiter=',')
