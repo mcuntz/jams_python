260906ea0e22f247371fdb7685a56ded1944b42c
Matthias Cuntz <matthias.cuntz@ufz.de>
2015-07-09 14:47:47 +0000

    Python: level1/excel.py - column and variable can be iterables;   if column and/or variable is None then the names are returned.
    git-svn-id: https://svn.ufz.de/svn/chs-svn/PYTHON_chs_lib@2224 20e3f476-f421-4892-ba25-73c5d2d2c40b

--- ufz/level1/excel.py
+++ ufz/level1/excel.py
@@ -9,33 +9,36 @@ __all__ = ['get_value_excel']
 
 def get_value_excel(excelfile, sheet, variable, column):
     """
-        Get the value in a column of a sheet in an excelfile
-        given a variable name that is looked-up in the column
-        "headerout (final)" in the same sheet.
+        Get a cell in a sheet of an Excel file
+        where the column is identified by the name in the first row
+        and the row is identified by the value in the column "headerout (final)".
 
+        Both, variable and column can be iterables.
 
-        Definition
-        ----------
-        def get_value_excel(excelfile, sheet, variable, column):
-
-
-        Input
-        -----
-        excelfile   filename of Excel file
-        sheet       Name of sheet in Excel file
-        variable    variable name as given in column "headerout (final)" in sheet
-        column      column name of the parameter wanted
+        If variable and/or column is None then the list of variables and columns is returned.
 
         
         Output
         ------
         content of the desired cell
+        if column or variable is iterable, output is list
+        if column and variable is iterable, output is list (variable) of list (columns)
+        if column and/or variable is not given, then a list of all variable and/or columns is returned
 
 
         Examples
         --------
-        mini = get_value_excel('CHS-measurements.xlsx', 'Forest Hohes Holz', 'BC1_Ptemp [degC]', 'Min')
+        mini = get_value_excel('CHS-measurements.xlsx', 'Logger Hohes Holz', 'BC1_Ptemp [degC]', 'Min')
         -> -20.0
+        mini, maxi = get_value_excel('CHS-measurements.xlsx', 'Logger Hohes Holz', 'BC1_Ptemp [degC]', ['Min', 'Max'])
+        -> -20.0, 50.0
+        min1, min2 = get_value_excel('CHS-measurements.xlsx', 'Logger Hohes Holz', ['BC1_Ptemp [degC]', 'Tair50HMP [degC]'], 'Min')
+        -> -20.0, -20.0
+        minmax1, minmax2 = get_value_excel('CHS-measurements.xlsx', 'Logger Hohes Holz', ['BC1_Ptemp [degC]', 'Tair50HMP [degC]'], ['Min', 'Max'])
+        -> [-20.0, 50.0], [-20.0, 50.0]
+        vars = get_value_excel('CHS-measurements.xlsx', 'Logger Hohes Holz', None, '')
+        columns = get_value_excel('CHS-measurements.xlsx', 'Logger Hohes Holz', '', None)
+        vars, columns = get_value_excel('CHS-measurements.xlsx', 'Logger Hohes Holz', None, None)
 
 
         License
@@ -62,24 +65,56 @@ def get_value_excel(excelfile, sheet, variable, column):
         History
         -------
         Written,  MC, Mar 2015
+        Modified, MC, Jul 2015 - column/variable can be iterable
+                               - column/variable can None, returning names
     """
     # open Excel file
     wb = xlrd.open_workbook(excelfile)
     # check sheet exists
     if sheet not in wb.sheet_names(): raise ValueError('No sheet'+sheet+' in file '+excelfile)
     sh = wb.sheet_by_name(sheet)
+    # Return varible and column names
+    columns   = sh.row_values(0,0)
+    variables = sh.col_values(columns.index('headerout (final)'), start_rowx=1, end_rowx=sh.nrows)
+    if (variable is None) and (column is None):
+        return [variables, columns]
+    elif (variable is None):
+        return variables
+    elif (column is None):
+        return columns
     # check columns exist
-    hh = ['headerout (final)', column]
+    hh = ['headerout (final)']
+    if isinstance(column, (list, tuple, np.ndarray)):
+        hh.extend(column)
+    else:
+        hh.append(column)
     for i in hh:
-        if i not in sh.row_values(0,0):
+        if i not in columns:
             raise ValueError('Column '+i+' not found in sheet '+sheet+' of file '+excelfile)
-    # read the columns
-    variables = sh.col_values(sh.row_values(0,0).index('headerout (final)'), start_rowx=1, end_rowx=sh.nrows)
-    parameter = sh.col_values(sh.row_values(0,0).index(column), start_rowx=1, end_rowx=sh.nrows)
-    if variable not in variables:
-        raise ValueError(variable+' not in column "headerout (final)" in sheet '+sheet+' of file '+excelfile)
-    # extract cell
-    out = parameter[variables.index(variable)]
+    # check columns
+    if isinstance(variable, (list, tuple, np.ndarray)):
+        for ivar in variable:
+            if ivar not in variables:
+                raise ValueError(ivar+' not in column "headerout (final)" in sheet '+sheet+' of file '+excelfile)
+    else:
+        if variable not in variables:
+            raise ValueError(variable+' not in column "headerout (final)" in sheet '+sheet+' of file '+excelfile)
+    # extract cells
+    if isinstance(column, (list, tuple, np.ndarray)) and isinstance(variable, (list, tuple, np.ndarray)):
+        out = list()
+        for ivar in variable:
+            out.append([ sh.col_values(columns.index(icol),
+                                       start_rowx=1, end_rowx=sh.nrows)[variables.index(ivar)] for icol in column ])
+    else:
+        if isinstance(column, (list, tuple, np.ndarray)):     # columns
+            out = [ sh.col_values(columns.index(icol),
+                                  start_rowx=1, end_rowx=sh.nrows)[variables.index(variable)] for icol in column ]
+        elif isinstance(variable, (list, tuple, np.ndarray)): # variables
+            out = [ sh.col_values(columns.index(column),
+                                  start_rowx=1, end_rowx=sh.nrows)[variables.index(ivar)] for ivar in variable ]
+        else:                                                 # scalars
+            parameter = sh.col_values(columns.index(column), start_rowx=1, end_rowx=sh.nrows)
+            out = parameter[variables.index(variable)]
     # relase Excel file
     del wb
     
@@ -90,3 +125,17 @@ def get_value_excel(excelfile, sheet, variable, column):
 if __name__ == '__main__':
     import doctest
     doctest.testmod(optionflags=doctest.NORMALIZE_WHITESPACE)
+
+    # base_folder = '/Volumes/Gruppen/tereno/CHS-Data'
+    # setup_chs = base_folder+'/CHS-measurements.xlsx'
+    # sheet = 'Logger Hohes Holz'
+    # v = 'Tair50HMP [degC]'
+    # print(get_value_excel(setup_chs, sheet, v, 'Min'))
+    # print(get_value_excel(setup_chs, sheet, v, ['Min','Max']))
+    # v = ['Tair50HMP [degC]', 'BC1_Ptemp [degC]']
+    # print(get_value_excel(setup_chs, sheet, v, 'Min'))
+    # print(get_value_excel(setup_chs, sheet, v, ['Min','Max']))
+    # # print(get_value_excel(setup_chs, 'Logger Hohes Holz', None, ''))
+    # print(get_value_excel(setup_chs, 'Logger Hohes Holz', '', None))
+    # # print(get_value_excel(setup_chs, 'Logger Hohes Holz', None, None))
+
