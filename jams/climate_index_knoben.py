37ca6c38c67d59813dd9cae06fce82579623b1e0
Juliane Mai <juliane.mai@uwaterloo.ca>
2020-02-19 23:45:15 -0500

    Enable additional input of snow (used to determine f_S)

--- jams/climate_index_knoben.py
+++ jams/climate_index_knoben.py
@@ -2,9 +2,12 @@
 from __future__ import division, absolute_import, print_function
 import numpy as np
 
-def climate_index_knoben(time, precip, tave, pet, color=True, indicators=False):
+def climate_index_knoben(time, precip, tave, pet, snow=None, color=True, indicators=False):
     """
     Calculates climate index (color and/or indicators) based on Knoben et al. (2018).
+
+    Addition: The fraction of precipitation as snow is based on snow time series given rather 
+    than using Eq. 3 of Knoben et al. (2018).
 
     The daily precipitation, avergage daily temperature, and potential evapotranspiration 
     is converted into climate indicators aridity I_m, Seasonality I_m,r, and Precipitation 
@@ -19,19 +22,21 @@ def climate_index_knoben(time, precip, tave, pet, color=True, indicators=False):
 
     Definition
     ----------
-    climate_index_knoben(time, precip, tave, pet, color=True, indicators=False):
+    climate_index_knoben(time, precip, tave, pet, snow=None, color=True, indicators=False):
 
 
     Input
     -----
     time         array of datetime objects of time stamps
-    precip       array of daily precipitation
+    precip       array of daily precipitation (snow+rain)
     tave         array of daily average temperature
     pet          array of daily potential evapotranspiration
 
 
     Options
     -------
+    snow         array of daily average snow (uses this estimate rather 
+                 than Eq. 3 of Knoben et al. (2018).
     color        triplet of RGB values for 
                  - aridity (red), 
                  - seasonality (green), and 
@@ -117,15 +122,15 @@ def climate_index_knoben(time, precip, tave, pet, color=True, indicators=False):
     df = pd.DataFrame(data, columns = ['precip', 'tave', 'pet'], index=pd.DatetimeIndex(time), dtype=float)
 
     # monthly sum of precip
-    precip_total_monthly     = df.precip.resample("M").agg(['sum'])                      # 12 x 61 values
+    precip_total_monthly     = df.precip.resample("M").agg(['sum'])                                  # 12 x 61 values
     precip_total_monthly_ave = precip_total_monthly.groupby(precip_total_monthly.index.month).mean() # 12 values
 
     # average monthly temperature
-    tave_ave_monthly     = df.tave.resample("M").agg(['mean'])                         # 12 x 61 values
-    tave_ave_monthly_ave = tave_ave_monthly.groupby(tave_ave_monthly.index.month).mean()       # 12 values
+    tave_ave_monthly     = df.tave.resample("M").agg(['mean'])                                       # 12 x 61 values
+    tave_ave_monthly_ave = tave_ave_monthly.groupby(tave_ave_monthly.index.month).mean()             # 12 values
 
     # average monthly pet
-    pet_total_monthly     = df.pet.resample("M").agg(['sum'])                           # 12 x 61 values
+    pet_total_monthly     = df.pet.resample("M").agg(['sum'])                                        # 12 x 61 values
     pet_total_monthly_ave = pet_total_monthly.groupby(pet_total_monthly.index.month).mean()          # 12 values
 
     # >>>>>>>>>>>>>> TODO: write these three to file
@@ -153,11 +158,17 @@ def climate_index_knoben(time, precip, tave, pet, color=True, indicators=False):
     #       2 = seasonal
     I_mr = np.max(MI) - np.min(MI)
 
-    # Equation 3 in Knoben et al. (2018)
-    #  ---> Blue  = Fraction of precipitation as snow f_S
-    #       0 = no snow
-    #       1 = all snow
-    f_S = np.sum( precip_total_monthly_ave.values[tave_ave_monthly_ave.values <= 0.0] ) / np.sum( precip_total_monthly_ave.values )
+    if (snow is None):
+        # Equation 4 in Knoben et al. (2018)
+        #  ---> Blue  = Fraction of precipitation as snow f_S
+        #       0 = no snow
+        #       1 = all snow
+        f_S = np.sum( precip_total_monthly_ave.values[tave_ave_monthly_ave.values <= 0.0] ) / np.sum( precip_total_monthly_ave.values )
+    else:
+        # if snow farction is given, ratio of total snow amount and total precipitation amount is used to derive snow fraction
+        f_S = np.sum( snow ) / np.sum( precip_total_monthly.values )
+        # print('snow amount: ',np.sum( snow ))
+        # print('prec amount: ',np.sum( precip_total_monthly.values ))
 
     # print("Aridity I_m                           = ", I_m)
     # print("Seasonality I_m,r                     = ", I_mr)
