58e1e9f1559c71d76297cd2a37f349326f8b1943
Matthias Cuntz <mc@macu.de>
2022-09-14 23:20:13 +0200

    Respect latency of provision of ERA5 and ERA5-Land for current year

--- jams/get_era5.py
+++ jams/get_era5.py
@@ -19,15 +19,18 @@ You can (re-)download the data from this site later as well.
 
 Written  Matthias Cuntz, Jan 2019 - from get_era_interim.py
 Modified Stephan Thober, Mar 2020 - added era5-land capability
-         Matthias Cuntz, Jun 2020 - return correct file list and not the list
-                                    of projected filenames for chosen area
-                                  - input variable list with MuSICA variables
-                                    as default
-                                  - finalised era5-land capability
+         Matthias Cuntz, Jun 2020
+             - return correct file list and not the list of projected filenames
+               for chosen area
+             - input variable list with MuSICA variables as default
+             - finalised era5-land capability
          Matthias Cuntz, Feb 2021 - bug in single point -> lat1 > lat2
          Matthias Cuntz, Feb 2021 - added grib format
                                   - default download era5-land in grib format
          Matthias Cuntz, Sep 2021 - no default area: -a must be given.
+         Matthias Cuntz & Jerome Ogee, Sep 2022
+             - respect latency of provision of ERA5 and ERA5-Land for
+               current year
 
 
 --------------------------------------------------------
@@ -86,6 +89,7 @@ Further help comes from
 from __future__ import division, absolute_import, print_function
 import os
 import glob
+import datetime as dt
 
 
 __all__ = ['get_era5']
@@ -104,8 +108,8 @@ def _get_era5_single_level5(variables, date, time, area, target,
     import cdsapi
 
     # check reanalysis mode
-    retrieve_name = {'era5':      'reanalysis-era5-single-levels',
-                     'era5land':  'reanalysis-era5-land',
+    retrieve_name = {'era5': 'reanalysis-era5-single-levels',
+                     'era5land': 'reanalysis-era5-land',
                      'era5-land': 'reanalysis-era5-land'}
     if reanalysis_model not in retrieve_name.keys():
         estr = 'Value of variable reanalysis_model'
@@ -126,12 +130,12 @@ def _get_era5_single_level5(variables, date, time, area, target,
     server = cdsapi.Client()
 
     request = {
-        'format':   output_format,  # grib or netcdf
-        'variable': variables,      # ['10m_v_component_of_wind', ...]
-        'date':     date,           # "2010-01-01", "2010-01-01/2015-12-31"
-        'time':     time,           # "[00:00, 01:00, 02:00, ..., 23:00]
-        'area':     area,           # North, West, South, East within
-                                    # "90/-180/-90/180" or [90, -180, -90, 180]
+        'format': output_format,  # grib or netcdf
+        'variable': variables,    # ['10m_v_component_of_wind', ...]
+        'date': date,             # "2010-01-01", "2010-01-01/2015-12-31"
+        'time': time,             # "[00:00, 01:00, 02:00, ..., 23:00]
+        'area': area,             # North, West, South, East within
+                                  # "90/-180/-90/180" or [90, -180, -90, 180]
     }
 
     if reanalysis_model == 'era5':
@@ -304,27 +308,33 @@ def get_era5(vars=['10m_u_component_of_wind', '10m_v_component_of_wind',
     This file is part of the JAMS Python package,
     distributed under the MIT License.
 
-    Copyright (c) 2019 Matthias Cuntz - mc (at) macu (dot) de
+    Copyright (c) 2019-2022 Matthias Cuntz - mc (at) macu (dot) de
 
 
     History
     -------
     Written  Matthias Cuntz, Jan 2019 - from get_era_interim.py
-    Modified Matthias Cuntz, Dec 2019 - default area (global) was not working:
-                                        used == instead of =
-             Stephan Thober, Mar 2020 - added optional reanalysis_model
-                                        argument to download era5land
-             Matthias Cuntz, Jun 2020 - allow name era5-land
-                                      - return correct file list and not the
-                                        list of projected filenames for chosen
-                                        area
-                                      - input variable list with MuSICA
-                                        variables as default
-                                      - finalised era5-land capability
-                                      - use numpydoc format
+    Modified Matthias Cuntz, Dec 2019
+                 - default area (global) was not working:
+                   used == instead of =
+             Stephan Thober, Mar 2020
+                 - added optional reanalysis_model argument to download
+                   era5land
+             Matthias Cuntz, Jun 2020
+                 - allow name era5-land
+                 - return correct file list and not the list of projected
+                   filenames for chosen area
+                 - input variable list with MuSICA variables as default
+                 - finalised era5-land capability
+                 - use numpydoc format
              Matthias Cuntz, Feb 2021 - bug in single point -> lat1 > lat2
-             Matthias Cuntz, Feb 2021 - added grib format
-                                      - download era5-land in grib format
+             Matthias Cuntz, Feb 2021
+                 - added grib format
+                 - download era5-land in grib format
+             Matthias Cuntz & Jerome Ogee, Sep 2022
+                 - respect latency of provision of ERA5 and ERA5-Land for
+                   current year
+
     """
     # Check parameters
     # reanalysis model
@@ -336,12 +346,14 @@ def get_era5(vars=['10m_u_component_of_wind', '10m_v_component_of_wind',
         minyear    = 1979
         if not output_format:
             output_format = 'netcdf'
+        latency = 10  # days before today for product update
     else:
         rmodel     = 'era5land'
         resolution = 0.1
         minyear    = 1981
         if not output_format:
             output_format = 'grib'
+        latency = 100
     output_format = output_format.lower()
     if output_format == 'netcdf':
         suffix = '.nc'
@@ -358,14 +370,14 @@ def get_era5(vars=['10m_u_component_of_wind', '10m_v_component_of_wind',
         estr = 'area format is lat,lon or NorthLat/WestLon/SouthLat/EastLon.'
         assert len(sarea) == 4, estr
         estr = 'area must be in 90/-180/-90/180'
-        assert float(sarea[0]) <= 90.,   estr
+        assert float(sarea[0]) <= 90., estr
         assert float(sarea[1]) >= -180., estr
-        assert float(sarea[2]) >= -90.,  estr
-        assert float(sarea[3]) <= 180.,  estr
+        assert float(sarea[2]) >= -90., estr
+        assert float(sarea[3]) <= 180., estr
         estr = 'area format is lat,lon or NorthLat/WestLon/SouthLat/EastLon.'
-        assert float(sarea[0]) >= float(sarea[2])+resolution, estr
+        assert float(sarea[0]) >= float(sarea[2]) + resolution, estr
         estr = 'area format is lat,lon or NorthLat/WestLon/SouthLat/EastLon.'
-        assert float(sarea[1])+resolution <= float(sarea[3]), estr
+        assert float(sarea[1]) + resolution <= float(sarea[3]), estr
     else:
         estr = 'area format is lat,lon or NorthLat/WestLon/SouthLat/EastLon.'
         assert ',' in area, estr
@@ -374,10 +386,10 @@ def get_era5(vars=['10m_u_component_of_wind', '10m_v_component_of_wind',
         assert len(sarea) == 2, estr
         lat, lon = sarea
         # single point does not work. Needs to encompass an actual grid point.
-        area = (str(float(lat)+resolution/2.) + '/'
-                + str(float(lon)-resolution/2.) + '/'
-                + str(float(lat)-resolution/2.) + '/'
-                + str(float(lon)+resolution/2.))
+        area = (str(float(lat) + resolution / 2.) + '/'
+                + str(float(lon) - resolution / 2.) + '/'
+                + str(float(lat) - resolution / 2.) + '/'
+                + str(float(lon) + resolution / 2.))
 
     # years
     if years:
@@ -402,15 +414,15 @@ def get_era5(vars=['10m_u_component_of_wind', '10m_v_component_of_wind',
         os.makedirs(path)
 
     # Single year output files
-    hasyrs     = list(range(yearstart, yearend+1))
+    hasyrs     = list(range(yearstart, yearend + 1))
     targetera5 = [ path + '/' + rmodel + '_' + area.replace('/', '_')
                    + '_{:04d}'.format(yy) + suffix for yy in hasyrs ]
     targetyrs  = targetera5[:]
 
     # Check existing files
-    files = glob.glob(path+'/*.nc*') + glob.glob(path+'/*.gr*b*')
+    files = glob.glob(path + '/*.nc*') + glob.glob(path + '/*.gr*b*')
     files = [ ff for ff in files
-              if os.path.basename(ff).startswith(rmodel+'_') ]
+              if os.path.basename(ff).startswith(rmodel + '_') ]
     if (len(files) > 0) and (not override):
         ilat1, ilon1, ilat2, ilon2 = [ float(i) for i in area.split('/') ]
         for ff in files:
@@ -419,8 +431,8 @@ def get_era5(vars=['10m_u_component_of_wind', '10m_v_component_of_wind',
             lon1 = float(fs[-4])
             lat2 = float(fs[-3])
             lon2 = float(fs[-2])
-            if ((ilat1 <= lat1) and (ilat2 >= lat2) and
-                (ilon1 >= lon1) and (ilon2 <= lon2)):
+            if ( (ilat1 <= lat1) and (ilat2 >= lat2) and
+                 (ilon1 >= lon1) and (ilon2 <= lon2) ):
                 fs  = fs[-1]
                 yrs = fs[:fs.rfind('.')]
                 if '-' in yrs:
@@ -429,7 +441,7 @@ def get_era5(vars=['10m_u_component_of_wind', '10m_v_component_of_wind',
                 else:
                     # check single year file
                     yr2 = yr1 = int(yrs)
-                for yr in range(yr1, yr2+1):
+                for yr in range(yr1, yr2 + 1):
                     if (yr >= yearstart) and (yr <= yearend):
                         if yr in hasyrs:
                             ii = hasyrs.index(yr)
@@ -449,6 +461,7 @@ def get_era5(vars=['10m_u_component_of_wind', '10m_v_component_of_wind',
 
     # get reanalysis
     if len(hasyrs) > 0:
+        today = dt.date.today()
         if len(hasyrs) == 1:
             yy = 0
             target = targetyrs[yy]
@@ -457,11 +470,24 @@ def get_era5(vars=['10m_u_component_of_wind', '10m_v_component_of_wind',
             #    A time period as "2010-01-01/2015-12-31"
             startdate = '{:04d}-{:02d}-{:02d}'.format(hasyrs[yy], 1, 1)
             lastdate  = '{:04d}-{:02d}-{:02d}'.format(hasyrs[yy], 12, 31)
-            dates = (startdate+"/"+lastdate)
-            print('Retrieve ', dates, target)
-            data_file_era5 = _get_era5_single_level5(
-                vars, dates, times, area, target, reanalysis_model=rmodel,
-                output_format=output_format)
+            # ERA5 has a latency of about 5 days; ERA5-Land about three
+            # months. So if current year is chosen, we limit to 10 days
+            # before today for ERA5 and to 100 days before today for
+            # ERA5-Land.
+            downyear = True
+            if hasyrs[yy] == today.year:
+                lastdate = today - dt.timedelta(days=latency)
+                if hasyrs[yy] != lastdate.year:
+                    print(f'{reanalysis_model} not available yet for this'
+                          f' year')
+                    downyear = False
+                lastdate = lastdate.isoformat()
+            if downyear:
+                dates = (startdate + "/" + lastdate)
+                print('Retrieve ', dates, target)
+                data_file_era5 = _get_era5_single_level5(
+                    vars, dates, times, area, target, reanalysis_model=rmodel,
+                    output_format=output_format)
         else:
             # Do one by year because CDS allows only download of 100000 items
             # at a time.
@@ -475,9 +501,22 @@ def get_era5(vars=['10m_u_component_of_wind', '10m_v_component_of_wind',
                 #    A time period as "2010-01-01/2015-12-31"
                 startdate = '{:04d}-{:02d}-{:02d}'.format(hasyrs[yy], 1, 1)
                 lastdate  = '{:04d}-{:02d}-{:02d}'.format(hasyrs[yy], 12, 31)
-                dates = (startdate+"/"+lastdate)
-                datestarget += [(dates, target)]
-                print('Pool ', dates, target)
+                # ERA5 has a latency of about 5 days; ERA5-Land about three
+                # months. So if current year is chosen, we limit to 10 days
+                # before today for ERA5 and to 100 days before today for
+                # ERA5-Land.
+                downyear = True
+                if hasyrs[yy] == today.year:
+                    lastdate = today - dt.timedelta(days=latency)
+                    if hasyrs[yy] != lastdate.year:
+                        print(f'{reanalysis_model} not available yet for this'
+                              f' year')
+                        downyear = False
+                    lastdate = lastdate.isoformat()
+                if downyear:
+                    dates = (startdate + "/" + lastdate)
+                    datestarget += [(dates, target)]
+                    print('Pool ', dates, target)
             from functools import partial
             getit = partial(_get_era5_single_level5_datestarget, vars, times,
                             area, reanalysis_model=rmodel,
@@ -497,53 +536,53 @@ if __name__ == "__main__":
 
     import argparse
 
-    area             = ''
-    oformat          = ''
-    override         = False
-    path             = '.'
+    area = ''
+    oformat = ''
+    override = False
+    path = '.'
     reanalysis_model = 'era5'
-    varis            = '10m_u_component_of_wind'
-    varis            = varis + ',' + '10m_v_component_of_wind'
-    varis            = varis + ',' + '2m_temperature,2m_dewpoint_temperature'
-    varis            = varis + ',' + 'total_precipitation,snowfall'
-    varis            = varis + ',' + 'surface_pressure'
-    varis            = varis + ',' + 'surface_solar_radiation_downwards'
-    varis            = varis + ',' + 'surface_thermal_radiation_downwards'
-    years            = None
+    varis = ('10m_u_component_of_wind,'
+             '10m_v_component_of_wind,'
+             '2m_temperature,2m_dewpoint_temperature,'
+             'total_precipitation,snowfall,'
+             'surface_pressure,'
+             'surface_solar_radiation_downwards,'
+             'surface_thermal_radiation_downwards')
+    years = None
 
-    parser           = argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
-        description='''
-        Download ERA5 or ERA5-Land data from Copernicus Climate Data Store
-        https://climate.copernicus.eu/climate-data-store.
-        ''')
-    hstr = 'area format as either lat,lon or NorthLat/WestLon/SouthLat/EastLon'
-    hstr = hstr + ', e.g. global 90/-180/-90/180, mandatory.'
+        description=('Download ERA5 or ERA5-Land data from Copernicus Climate'
+                     ' Data Store'
+                     ' https://climate.copernicus.eu/climate-data-store.'))
+    hstr = ('area format as either lat,lon or'
+            ' NorthLat/WestLon/SouthLat/EastLon, e.g. global 90/-180/-90/180,'
+            ' mandatory.')
     parser.add_argument('-a', '--area', action='store', default=area,
                         dest='area', metavar='area', help=hstr)
-    hstr  = 'Output format netcdf or grib. (default: netcdf if era5,'
-    hstr += ' grib if era5-land).'
+    hstr  = ('Output format netcdf or grib. (default: netcdf if era5,'
+             ' grib if era5-land).')
     parser.add_argument('-f', '--format', action='store', default=oformat,
                         dest='oformat', metavar='format', help=hstr)
-    hstr = 'Do not check that output file already exists that includes'
-    hstr = hstr + ' request. Override existing output file (default: False).'
+    hstr = ('Do not check that output file already exists that includes'
+            ' request. Override existing output file (default: False).')
     parser.add_argument('-o', '--override', action='store_true',
                         default=override, dest='override',
                         help=hstr)
     hstr = 'Output directory (default: current directory ".").'
     parser.add_argument('-p', '--path', action='store', default=path,
                         dest='path', metavar='path', help=hstr)
-    hstr = 'Reanalyis model to download, either: era5, era5-land or era5land'
-    hstr = hstr + ' (default: era5)'
+    hstr = ('Reanalyis model to download, either: era5, era5-land or era5land'
+            ' (default: era5)')
     parser.add_argument('-r', '--reanalyis-model', action='store',
                         default=reanalysis_model, dest='reanalyis_model',
                         metavar='reanalysis_model', help=hstr)
-    hstr = 'Comma-separated variable list var1,var2,... (default: forcing'
-    hstr = hstr + ' variables of ecosystem model MuSICA: '+varis+').'
+    hstr = (f'Comma-separated variable list var1,var2,... (default: forcing'
+            f' variables of ecosystem model MuSICA: {varis}.')
     parser.add_argument('-v', '--variables', action='store', default=varis,
                         dest='varis', metavar='variables', help=hstr)
-    hstr = 'years format is startyear,endyear (default: 1979,current-1 for'
-    hstr = hstr + ' ERA5 and 1981,current-1 for ERA5-Land.)'
+    hstr = ('years format is startyear,endyear (default: 1979,current-1 for'
+            ' ERA5 and 1981,current-1 for ERA5-Land.)')
     parser.add_argument('-y', '--years', action='store', default=years,
                         dest='years', metavar='years', help=hstr)
 
@@ -559,11 +598,11 @@ if __name__ == "__main__":
     del parser, args
 
     if not area:
-        print('area as either lat,lon or NorthLat/WestLon/SouthLat/EastLon')
-        print('must be given with -a option, e.g. --area="90/-180/-90/180"')
-        print('or a specific location like FR-Hes: -a 48.6742167,7.0646167.')
-        import sys
-        sys.exit()
+        raise ValueError('area as either lat,lon or'
+                         ' NorthLat/WestLon/SouthLat/EastLon must be given'
+                         ' with -a option, e.g. --area="90/-180/-90/180"'
+                         ' or a specific location like FR-Hes:'
+                         ' -a 48.6742167,7.0646167.')
 
     # years
     if years:
@@ -592,6 +631,6 @@ if __name__ == "__main__":
     print('Files: ', era5files)
 
     t2    = ptime.time()
-    strin = ('[m]: {:.1f}'.format((t2-t1)/60.) if (t2-t1) > 60. else
-             '[s]: {:d}'.format(int(t2-t1)))
+    strin = ('[m]: {:.1f}'.format((t2 - t1) / 60.) if (t2 - t1) > 60. else
+             '[s]: {:d}'.format(int(t2 - t1)))
     print('Time elapsed', strin)
